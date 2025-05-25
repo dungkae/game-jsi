7396f3c23df77603b1b821d51dd7531f9921d561
dungkae <dungkae280@gmail.com>
2025-05-25 10:13:40 +0700

    Connect to firebase

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -29,8 +29,37 @@ const db = getFirestore(app);
 
 // Initialize Cloud Firestore and get a reference to the service
 
-const querySnapshot = await getDocs(collection(db, "Skin"));
-querySnapshot.forEach((doc) => {
-  // doc.data() is never undefined for query doc snapshots
-  console.log(doc.id, " => ", doc.data());
-  });
+async function displayProducts() {
+    const productCollection = collection(db, "skin");
+    const querySnapshot = await getDocs(productCollection);
+
+    const wrapper = document.querySelector("game-list");
+
+    querySnapshot.forEach((doc) => {
+        const product = doc.data();
+
+        // Create a swiper-slide element
+        const slide = document.createElement("div");
+        slide.classList.add("game");
+
+        // Add product content to the slide
+        slide.innerHTML = `
+            
+                        <img src="https://imageio.forbes.com/specials-images/imageserve/613766b74881e4e67926a2c3/0x0.jpg?format=jpg&height=900&width=1600&fit=bounds" alt="Zedd x Spectrum Skin"/>
+                        <h4>Zedd x Spectrum Skin</h4>
+                        <p>Là bộ súng đắt và đẹp nhất trong game valorant</p>
+                        <p class="price">$100.00 USD</p>
+                        <a href="/html/game-detail/game3.html" class="buy-button">Mua Ngay</a>
+                    
+        `;
+
+        // Append the slide to the swiper-wrapper
+        wrapper.appendChild(slide);
+    });
+
+    // Reinitialize Swiper after adding slides
+
+}
+
+// Call the function to display products
+displayProducts();
