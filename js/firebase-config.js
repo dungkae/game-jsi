a086c9bda6e7bc6f5496e76bb14dbb5cc28f0d3d
Ke Dat <datmartin10@gmail.com>
2025-06-01 09:05:32 +0700

    Fix script at firebase config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -30,34 +30,33 @@ const db = getFirestore(app);
 // Initialize Cloud Firestore and get a reference to the service
 
 async function displayProducts() {
-    const productCollection = collection(db, "skin");
-    const querySnapshot = await getDocs(productCollection);
+  const productCollection = collection(db, "Skin");
+  const querySnapshot = await getDocs(productCollection);
 
-    const wrapper = document.querySelector("game-list");
+  const wrapper = document.querySelector(".game-list-1");
 
-    querySnapshot.forEach((doc) => {
-        const product = doc.data();
+  querySnapshot.forEach((doc) => {
+    const product = doc.data();
+    console.log("🚀 ~ querySnapshot.forEach ~ product:", product)
 
-        // Create a swiper-slide element
-        const slide = document.createElement("div");
-        slide.classList.add("game");
+    // Create a swiper-slide element
+    const slide = document.createElement("div");
+    slide.classList.add("game");
 
-        // Add product content to the slide
-        slide.innerHTML = `
-            
-                        <img src="https://imageio.forbes.com/specials-images/imageserve/613766b74881e4e67926a2c3/0x0.jpg?format=jpg&height=900&width=1600&fit=bounds" alt="Zedd x Spectrum Skin"/>
-                        <h4>Zedd x Spectrum Skin</h4>
-                        <p>Là bộ súng đắt và đẹp nhất trong game valorant</p>
-                        <p class="price">$100.00 USD</p>
-                        <a href="/html/game-detail/game3.html" class="buy-button">Mua Ngay</a>
-                    
+    // Add product content to the slide
+    slide.innerHTML = `
+                <img src="https://imageio.forbes.com/specials-images/imageserve/613766b74881e4e67926a2c3/0x0.jpg?format=jpg&height=900&width=1600&fit=bounds" alt="Zedd x Spectrum Skin"/>
+                <h4>Zedd x Spectrum Skin</h4>
+                <p>Là bộ súng đắt và đẹp nhất trong game valorant</p>
+                <p class="price">$100.00 USD</p>
+                <a href="/html/game-detail/game3.html" class="buy-button">Mua Ngay</a>
         `;
 
-        // Append the slide to the swiper-wrapper
-        wrapper.appendChild(slide);
-    });
+    // Append the slide to the swiper-wrapper
+    wrapper.appendChild(slide);
+  });
 
-    // Reinitialize Swiper after adding slides
+  // Reinitialize Swiper after adding slides
 
 }
 
