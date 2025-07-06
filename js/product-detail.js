365d1b2763e0e2e5ed121077151522cc092569b7
Ke Dat <datmartin10@gmail.com>
2025-07-06 09:26:54 +0700

    Get product details from firestore

--- js/product-detail.js
+++ js/product-detail.js
@@ -1,2 +1,51 @@
-var gameName = location.search.split('game=')[1]
-console.log("🚀 ~ gameName:", gameName)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  orderBy,
+  limit,
+  getDocs,
+  doc,
+  getDoc,
+} from "https://www.gstatic.com/firebasejs/11.7.3/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC9YCJlFvmjvheehAhX7Rwe3BckM9E1558",
+  authDomain: "dungkae1.firebaseapp.com",
+  projectId: "dungkae1",
+  storageBucket: "dungkae1.firebasestorage.app",
+  messagingSenderId: "390014988200",
+  appId: "1:390014988200:web:c31c49ebe162c98c61b79d",
+  measurementId: "G-7KTZPS260J"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+var skinName = location.search.split("game=")[1];
+async function getProductDetail() {
+  const productCollection = query(
+    collection(db, "Skin"),
+    where("url", "==", skinName)
+  );
+  const querySnapshot = await getDocs(productCollection);
+
+  querySnapshot.forEach((doc) => {
+    const product = doc.data();
+    console.log("🚀 ~ querySnapshot.forEach ~ product:", product);
+    
+  });
+
+}
+
+if (skinName) {
+  getProductDetail();
+}
